e16ee77a14de80134d4e4a5256eea2ae4b5bc6e3
Walicen Dalazuana <walicen.r@gmail.com>
2022-01-20 21:58:02 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    console.log("Hello")
+
+    let oneButton = document.getElementById("one")
+    oneButton.addEventListener("click", (e)=>{
+        console.log("Clicou no 1")
+        // adicionar o 1 no campo de texto;
+    })
+
+}
